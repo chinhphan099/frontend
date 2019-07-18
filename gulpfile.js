62738b337121472c4143eb980db0257168f44d5e
Chinh Phan <chinh.phan@dfo.global>
2019-07-18 09:43:52 +0700

    package

--- gulpfile.js
+++ gulpfile.js
@@ -187,14 +187,8 @@ gulp.task('webserver', () =>
 		port: process.env.PORT || 5000
 	})
 );
-gulp.task('serveprod', function() {
-	connect.server({
-		root: PUB_DIR.root,
-		port: process.env.PORT || 5000,
-		livereload: false
-	});
-});
 
 gulp.task('server', ['watch', 'webserver']);
 gulp.task('build', ['less', 'pug', 'pugdata', 'imagemin', 'scripts', 'jsguide', 'libs', 'copyAssets', 'watch']);
 gulp.task('default', ['clean'], () => { gulp.run(['build', 'server']); });
+gulp.task('deploy', ['webserver']);
